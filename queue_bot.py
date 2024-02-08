7143bd48889ee8cc582dd54166cedf5b0ab2b27b
Dmitry Ivanov <mail@skazzka.com>
2024-02-08 22:12:01 +0100

    Allow to run queue checker in standalone mode
    Make it accept security code and region via arguments.
    
    $ python3 queue_bot.py --subdomain berlin --order_id 238170 --code 3A56F4A5 --every_hours 2

--- queue_bot.py
+++ queue_bot.py
@@ -2,7 +2,7 @@ import time
 import os
 import argparse 
 
-from queue_class import QueueChecker
+from core.queue_checker import QueueChecker
 
 # kdmid_subdomain = 'madrid' 
 # order_id = '130238' 
@@ -14,51 +14,40 @@ kdmid_subdomain = 'warsaw'
 order_id = '85914'
 code = '824D737D'
 
-kdmid_subdomain = 'barcelona' 
-order_id = '205619' 
-code = '8367159E' 
-
-
-every_hours = 3
-
 def run(queue_checker, every_hours): 
 	success = False
 	while not success:
-	    if not os.path.isfile(queue_checker.order_id+"_"+queue_checker.code+"_success.txt"): 
-	        queue_checker.check_queue()
+	    if not os.path.isfile(queue_checker.order_id+"_"+queue_checker.code+"_success.json"):
+	        queue_checker.check_queue(kdmid_subdomain, order_id, code)
 	        time.sleep(every_hours*3600)
 	    else: 
 	        print('file exists, exiting')
 	        success = True
 
-queue_checker = QueueChecker(kdmid_subdomain, order_id, code)
- 	
-run(queue_checker, 3)
+if __name__ == '__main__':
 
+	parser = argparse.ArgumentParser(description='Parameters for checking')
 
+	parser.add_argument('--subdomain',
+	                       type=str, required=True,
+	                       help='The city where the consulate is situated')
+ 	
+	parser.add_argument('--order_id',
+	                       type=str, required=True,
+	                       help='Номер заявки')
+ 	
+	parser.add_argument('--code',
+	                       type=str, required=True,
+	                       help='Защитный код')
+ 	
+	parser.add_argument('--every_hours',
+	                       type=int, default=3,
+	                       help='Every n hours to check the queue, default 3')
 
-# if __name__ == '__main__':
-# 		
-#  	parser = argparse.ArgumentParser(description='Parameters for checking')
+	args = parser.parse_args()
 
-#  	parser.add_argument('--subdomain',
-# 	                       type=str, required=True,
-# 	                       help='The city where the consulate is situated')
- 	
-#  	parser.add_argument('--order_id',
-# 	                       type=str, required=True,
-# 	                       help='Номер заявки')
- 	
-#  	parser.add_argument('--code',
-# 	                       type=str, required=True, 
-# 	                       help='Защитный код')
- 	
-#  	parser.add_argument('--every_hours',
-# 	                       type=int, default=3,
-# 	                       help='Every n hours to check the queue, default 2')
- 	
-#  	args = parser.parse_args()
-# 	
-#  	queue_checker = QueueChecker(args.subdomain, args.order_id, args.code)
-# 	
-#  	run(queue_checker, args.every_hours)
+	kdmid_subdomain, order_id, code = args.subdomain, args.order_id, args.code
+
+	queue_checker = QueueChecker()
+
+	run(queue_checker, args.every_hours)
