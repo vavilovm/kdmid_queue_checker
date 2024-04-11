f58e65d85652084655508d5c13aaca7156ff8f1d
Mark Vavilov <vavilovmark@gmail.com>
2024-04-11 22:16:12 +0200

    support other os

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
-
-TESSERACT_PATH = '/usr/bin/tesseract'
+import os
+if os.name == 'nt':
+    TESSERACT_PATH = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+elif os.name == 'Darwin':
+    TESSERACT_PATH = r'/usr/local/Cellar/tesseract/5.3.3/bin/tesseract'
+else:
+    TESSERACT_PATH = '/usr/bin/tesseract'
